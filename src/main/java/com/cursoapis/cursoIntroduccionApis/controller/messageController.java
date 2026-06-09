df4af4672068f7a0f05e2db39d0dd9432aa75abe
Pelloft <anthony-flores_@hotmail.com>
2026-06-09 17:41:11 -0500

    Creando metodos get, post, delete

--- src/main/java/com/cursoapis/cursoIntroduccionApis/controller/messageController.java
+++ src/main/java/com/cursoapis/cursoIntroduccionApis/controller/messageController.java
@@ -1,12 +1,10 @@
 package com.cursoapis.cursoIntroduccionApis.controller;
-
 import com.cursoapis.cursoIntroduccionApis.entity.Message;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/messages")
@@ -14,12 +12,34 @@ public class messageController {
     private List<Message> mensajes = new ArrayList<>();
 
     public messageController() {
-        mensajes.add(new Message(1, "Prueba desarorollo de ApiRestFull"));
-        mensajes.add(new Message(2, "Prueba desarorollo de ApiRestFull 2"));
+        mensajes.add(new Message(1, "Prueba 1 desarorollo de ApiRestFull"));
+        mensajes.add(new Message(2, "Prueba 2 desarorollo de ApiRestFull"));
+        mensajes.add(new Message(3, "Prueba 3 desarorollo de ApiRestFull"));
     }
 
     @GetMapping
     public List<Message> getMessages() {
         return mensajes;
     }
+
+    @GetMapping("/{id}")
+    public Message obtenerMensajePorId(@PathVariable int id){
+        Optional<Message> message = mensajes.stream()
+                .filter(m -> m.getId() == id)
+                .findFirst();
+        return message.orElse(null);
+    }
+
+    @PostMapping
+    public Message crearMensaje(@RequestBody Message mensaje){
+        mensajes.add(mensaje);
+        return mensaje;
+    }
+
+    @DeleteMapping("/{id}")
+    public Message eliminarMensaje(@PathVariable int id){
+        mensajes.removeIf(m -> m.getId() == id);
+        return null;
+    }
+
 }
